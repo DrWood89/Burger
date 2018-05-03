177271bdaf932ed414faf7817af21fd01a67fd4d
Wood Jerry Remelus <woodjremelus@gmail.com>
2018-05-02 20:01:01 -0600

    final

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom & Swiss", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bleu Bacon Burger", false);
